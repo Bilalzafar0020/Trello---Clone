6636fcf39570e4a6738c1e8c2ca6d5fdd7958d73
Bilalzafar0020 <bilalafridi0020@gmail.com>
2023-05-29 17:40:42 +0500

    Trello Clone

--- script.js
+++ script.js
@@ -1,61 +1,121 @@
 
-// Get all the div elements with the 'contenteditable' attribute
-const draggableElements = document.querySelectorAll('div[contenteditable="true"]');
+/*  ISSUes (1)  when all div are drap off the height did'nt adjust
+(2) not draging on empty list
+*/
 
-// Store the currently dragged element
-let draggedElement = null;
+const addButtonSelectors = ['.AddaCard', '.AddaCard2', '.AddaCard3']; // In this line, an array named addButtonSelectors is declared and assigned with three CSS selector strings: '.AddaCard', '.AddaCard2', and '.AddaCard3'.
 
-// Add event listeners to enable dragging
-draggableElements.forEach((element) => {
-  element.addEventListener('dragstart', dragStart);
-  element.addEventListener('dragend', dragEnd);
+addButtonSelectors.forEach((buttonSelector) => {
+  const addButton = document.querySelector(buttonSelector); //  in this foreach gets the document of all buttons
+  //  and assign eventlistner to all 
+  addButton.addEventListener('click', addCard);
 });
 
-// Add event listeners to enable dropping
-document.querySelectorAll('.cards1, .cards2, .cards3').forEach((list) => {
-  list.addEventListener('dragover', dragOver);
-  list.addEventListener('dragenter', dragEnter);
-  list.addEventListener('dragleave', dragLeave);
-  list.addEventListener('drop', drop);
-});
+function addCard(event) {
+  const addButton = event.target;
+  const list = addButton.parentNode; //  
 
-// Drag start event handler
+  const card = document.createElement('div');
+  card.contentEditable = true;
+  card.draggable = true;
+  card.className = 'card';
+  card.style.display = 'inline-block'; //  display to inline-block
+  card.style.wordWrap = 'break-word'; //  word-wrap property to break-word so that the word should break
+  card.style.minHeight = '2.7rem'; // Set min-height to 2rem
+  card.style.height = 'auto'; // Set height to auto so that the height hould increase ac ording to text
+  card.style.border = 'none';
+  card.style.outline = 'none';
+  card.style.borderRadius = '10px';
+  card.style.boxShadow = '5px 5px 5px 5px #81848b';
+  card.style.width = '18rem';
+  card.style.fontSize = '14px';
+  card.style.paddingTop = '10px';
+  card.style.textAlign = 'left';
+  card.style.boxShadow = '0 1px 0 rgba(9,30,66,.25)';
+  card.style.background = 'white';
+  card.style.marginTop = '10px';
+  card.style.marginLeft = '10px';
+  card.style.marginBottom = '3px';
+
+  //  the new card before the button so that the div should comes above the button
+  list.insertBefore(card, addButton);
+
+  const maxHeight = 500; // Set your desired maximum height here
+  const currentHeight = list.offsetHeight; // Get the current height of the list
+  const newHeight = currentHeight + card.offsetHeight; // Calculate the new height
+
+  if (newHeight > maxHeight) {
+    list.style.height = `${maxHeight}px`;
+    list.style.overflowY = 'scroll';
+  } else {
+    list.style.height = `${newHeight}px`;
+  }
+
+  addButton.scrollIntoView({ behavior: 'smooth', block: 'end' });
+
+// drag and drop + touch 
+card.addEventListener('dragstart', dragStart);
+  card.addEventListener('dragover', dragOver);
+  card.addEventListener('dragleave', dragLeave);
+  card.addEventListener('drop', drop);
+  card.addEventListener('touchstart', touchStart);
+  card.addEventListener('touchmove', touchMove);
+  card.addEventListener('touchend', touchEnd);
+}
+
+let dragCard = null;
+
 function dragStart(event) {
-  draggedElement = event.target;
+  const card = event.target;
+  dragCard = card;
+  card.style.opacity = '0.8';
   event.dataTransfer.effectAllowed = 'move';
-  event.dataTransfer.setData('text/plain', ''); // Required for Firefox compatibility
-  event.target.classList.add('dragging');
+  event.dataTransfer.setData('text/html', card.innerHTML);
 }
 
-// Drag end event handler
-function dragEnd(event) {
-  draggedElement = null;
-  event.target.classList.remove('dragging');
-}
-
-// Drag over event handler (to allow dropping)
 function dragOver(event) {
   event.preventDefault();
+  const card = event.target;
+  card.style.opacity = '0.8';
 }
 
-// Drag enter event handler (to provide visual feedback)
-function dragEnter(event) {
-  event.preventDefault();
-  event.target.classList.add('drag-enter');
-}
-
-// Drag leave event handler (to remove visual feedback)
 function dragLeave(event) {
-  event.target.classList.remove('drag-enter');
+  const card = event.target;
+  card.style.border = 'none';
 }
 
-// Drop event handler
 function drop(event) {
   event.preventDefault();
-  event.target.classList.remove('drag-enter');
-  
-  // Move the dragged element to the target container
-  event.target.appendChild(draggedElement);
+  const card = event.target;
+  card.style.border = 'none';
+  card.style.opacity = '1';
+  card.style.background='#ffffff'
+  const list = card.parentNode;
+  list.insertBefore(dragCard, card);
+  dragCard = null;
+}
+
+function touchStart(event) {
+  const card = event.target;
+  dragCard = card;
+  card.style.opacity = '0.8';
+}
+
+function touchMove(event) {
+  const card = event.target;
+  card.style.position = 'fixed';
+  card.style.top = event.touches[0].clientY + 'px';
+  card.style.left = event.touches[0].clientX + 'px';
+  event.preventDefault();
+}
+
+function touchEnd(event) {
+  const card = event.target;
+  card.style.position = 'static';
+  card.style.opacity = '1';
+  const list = card.parentNode;
+  list.insertBefore(dragCard, card);
+  dragCard = null;
 }
 
 
@@ -81,125 +141,120 @@ function drop(event) {
 
 
 
+// const addButtonSelectors = ['.AddaCard', '.AddaCard2', '.AddaCard3']; // In this line, an array named addButtonSelectors is declared and assigned with three CSS selector strings: '.AddaCard', '.AddaCard2', and '.AddaCard3'.
+
+// addButtonSelectors.forEach((buttonSelector) => {
+//   const addButton = document.querySelector(buttonSelector); //  in this foreach gets the document of all buttons
+//   //  and assign eventlistner to all 
+//   addButton.addEventListener('click', addCard);
+// });
+
+// function addCard(event) {
+//   const addButton = event.target;
+//   const list = addButton.parentNode; //  
+
+//   const card = document.createElement('div');
+//   card.contentEditable = true;
+//   card.draggable = true;
+//   card.className = 'card';
+//   card.style.display = 'inline-block'; //  display to inline-block
+//   card.style.wordWrap = 'break-word'; //  word-wrap property to break-word so that the word should break
+//   card.style.minHeight = '2.7rem'; // Set min-height to 2rem
+//   card.style.height = 'auto'; // Set height to auto so that the height hould increase ac ording to text
+//   card.style.border = 'none';
+//   card.style.outline = 'none';
+//   card.style.borderRadius = '10px';
+//   card.style.boxShadow = '5px 5px 5px 5px #81848b';
+//   card.style.width = '18rem';
+//   card.style.fontSize = '14px';
+//   card.style.paddingTop = '10px';
+//   card.style.textAlign = 'left';
+//   card.style.boxShadow = '0 1px 0 rgba(9,30,66,.25)';
+//   card.style.background = 'white';
+//   card.style.marginTop = '10px';
+//   card.style.marginLeft = '10px';
+//   card.style.marginBottom = '3px';
+
+//   //  the new card before the button so that the div should comes above the button
+//   list.insertBefore(card, addButton);
+
+//   const maxHeight = 500; // Set your desired maximum height here
+//   const currentHeight = list.offsetHeight; // Get the current height of the list
+//   const newHeight = currentHeight + card.offsetHeight; // Calculate the new height
+
+//   if (newHeight > maxHeight) {
+//     list.style.height = `${maxHeight}px`;
+//     list.style.overflowY = 'scroll';
+//   } else {
+//     list.style.height = `${newHeight}px`;
+//   }
+
+//   addButton.scrollIntoView({ behavior: 'smooth', block: 'end' });
+
+// // drag and drop + touch 
+// card.addEventListener('dragstart', dragStart);
+//   card.addEventListener('dragover', dragOver);
+//   card.addEventListener('dragleave', dragLeave);
+//   card.addEventListener('drop', drop);
+//   card.addEventListener('touchstart', touchStart);
+//   card.addEventListener('touchmove', touchMove);
+//   card.addEventListener('touchend', touchEnd);
+// }
+
+// let dragCard = null;
+
+// function dragStart(event) {
+//   const card = event.target;
+//   dragCard = card;
+//   card.style.opacity = '0.8';
+//   event.dataTransfer.effectAllowed = 'move';
+//   event.dataTransfer.setData('text/html', card.innerHTML);
+// }
+
+// function dragOver(event) {
+//   event.preventDefault();
+//   const card = event.target;
+//   card.style.opacity = '0.8';
+// }
+
+// function dragLeave(event) {
+//   const card = event.target;
+//   card.style.border = 'none';
+// }
+
+// function drop(event) {
+//   event.preventDefault();
+//   const card = event.target;
+//   card.style.border = 'none';
+//   card.style.opacity = '1';
+//   card.style.background='#ffffff'
+//   const list = card.parentNode;
+//   list.insertBefore(dragCard, card);
+//   dragCard = null;
+// }
+
+// function touchStart(event) {
+//   const card = event.target;
+//   dragCard = card;
+//   card.style.opacity = '0.8';
+// }
+
+// function touchMove(event) {
+//   const card = event.target;
+//   card.style.position = 'fixed';
+//   card.style.top = event.touches[0].clientY + 'px';
+//   card.style.left = event.touches[0].clientX + 'px';
+//   event.preventDefault();
+// }
+
+// function touchEnd(event) {
+//   const card = event.target;
+//   card.style.position = 'static';
+//   card.style.opacity = '1';
+//   const list = card.parentNode;
+//   list.insertBefore(dragCard, card);
+//   dragCard = null;
+// }
 
 
-    // // Check if data exists in local storage
-    // const savedData = localStorage.getItem('kanbanData');
-    // let kanbanData = savedData ? JSON.parse(savedData) : [];
 
-    // const listsContainer = document.getElementById('lists-container');
-    // const addListButton = document.getElementById('add-list-button');
-    // const addListInput = document.getElementById('add-list-input');
-    // let draggingCard = null;
-
-    // // Render the initial kanban board
-    // renderBoard();
-
-    // // Add event listener to the "Add a List" button
-    // addListButton.addEventListener('click', () => {
-    //   const newList = addListInput.value.trim();
-    //   if (newList) {
-    //     kanbanData.push({ title: newList, cards: [] });
-    //     saveData();
-    //     renderBoard();
-    //     addListInput.value = '';
-    //   }
-    // });
-
-    // // Render the kanban board
-    // function renderBoard() {
-    //   listsContainer.innerHTML = '';
-
-    //   kanbanData.forEach((list, listIndex) => {
-    //     const listElement = document.createElement('div');
-    //     listElement.classList.add('list');
-
-    //     const listTitle = document.createElement('div');
-    //     listTitle.classList.add('list-title');
-    //     listTitle.textContent = list.title;
-
-    //     const cardContainer = document.createElement('div');
-    //     cardContainer.classList.add('card-container');
-
-    //     const addCardButton = document.createElement('button');
-    //     addCardButton.classList.add('add-card-button');
-    //     addCardButton.textContent = 'Add a Card';
-
-    //     const inputElement = document.createElement('input');
-    //     inputElement.type = 'text';
-    //     inputElement.value = '';
-    //     inputElement.placeholder = 'Enter a card...';
-
-    //     // Event listener to add a card
-    //     addCardButton.addEventListener('click', () => {
-    //       const newCard = inputElement.value.trim();
-    //       if (newCard) {
-    //         list.cards.push(newCard);
-    //         saveData();
-    //         renderBoard();
-    //       }
-    //     });
-
-    //     // Event listener when input loses focus
-    //     inputElement.addEventListener('blur', () => {
-    //       const newCard = inputElement.value.trim();
-    //       if (newCard) {
-    //         list.cards.push(newCard);
-    //         saveData();
-    //         renderBoard();
-    //       }
-    //     });
-
-    //     // Render the cards
-    //     list.cards.forEach((card, cardIndex) => {
-    //       const cardElement = document.createElement('div');
-    //       cardElement.classList.add('card');
-    //       cardElement.textContent = card;
-    //       cardElement.draggable = true;
-
-    //       cardElement.addEventListener('dragstart', (event) => {
-    //         draggingCard = { listIndex, cardIndex };
-    //         cardElement.classList.add('dragging');
-    //       });
-
-    //       cardElement.addEventListener('dragend', () => {
-    //         draggingCard = null;
-    //         cardElement.classList.remove('dragging');
-    //       });
-
-    //       cardElement.addEventListener('dragover', (event) => {
-    //         event.preventDefault();
-    //       });
-
-    //       cardElement.addEventListener('drop', () => {
-    //         const { listIndex: sourceListIndex, cardIndex: sourceCardIndex } = draggingCard;
-
-    //         if (sourceListIndex === listIndex && sourceCardIndex === cardIndex) {
-    //           return;
-    //         }
-
-    //         const sourceCard = kanbanData[sourceListIndex].cards.splice(sourceCardIndex, 1)[0];
-    //         kanbanData[listIndex].cards.splice(cardIndex, 0, sourceCard);
-
-    //         saveData();
-    //         renderBoard();
-    //       });
-
-    //       cardContainer.appendChild(cardElement);
-    //     });
-
-    //     listElement.appendChild(listTitle);
-    //     listElement.appendChild(cardContainer);
-    //     listElement.appendChild(addCardButton);
-    //     listElement.appendChild(inputElement);
-
-    //     listsContainer.appendChild(listElement);
-    //   });
-    // }
-
-    // // Save the data to local storage
-    // function saveData() {
-    //   localStorage.setItem('kanbanData', JSON.stringify(kanbanData));
-    // }
-
-    
